36cdbff1b7fcee1f131642c6a5a9a3b2b4e7987f
Anna Matilainen <38183853+Redporkkana@users.noreply.github.com>
2024-08-23 16:47:36 +0100

    Update SyntheticDataPipeline.py
    Added comments to clarify the actions

--- SyntheticDataPipeline.py
+++ SyntheticDataPipeline.py
@@ -1,12 +1,13 @@
 # as pre-requisits, pip install pandas, numpy, faker and datetime
-
+# Import the libraries
 import pandas as pd
 from faker import Faker
 import numpy as np
 from datetime import datetime
 
+# instantiate Faker
 faker = Faker()
-def generate_data(num_records):
+def generate_data(num_records): # This function creates and populates a data frame of a given size with fields 'Name', 'Age', 'Email', 'Height' and 'Weight'
     data = {'Name': [], 'Age': [], 'Email': [], 'Height': [], 'Weight': []}
     for i in range(num_records):
         data['Name'].append(faker.name())
@@ -18,6 +19,6 @@ def generate_data(num_records):
 
 
 df = generate_data(2000)
-processed_df = df[(df['Age'] > 20) & (df['Age'] < 50)]
-unique_timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-processed_df.to_csv(f'unique_timestamp_{unique_timestamp}.csv', index=False)
+processed_df = df[(df['Age'] > 20) & (df['Age'] < 50)] # Filterining the data based on a certain criteria
+unique_timestamp = datetime.now().strftime('%Y%m%d%H%M%S')  
+processed_df.to_csv(f'unique_timestamp_{unique_timestamp}.csv', index=False) # Creating a .csv file with filtered data
